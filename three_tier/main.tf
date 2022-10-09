0d0c0abe3b9a856a9d6954dd241ecf29c6b16c61
gkranasinghe <gkranasinghe@gmail.com>
2022-10-09 13:12:45 +0530

    plan ok

--- three_tier/main.tf
+++ three_tier/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
 locals {
   instance_type = var.instance_type
-  aws_region      = var.aws_region
+  aws_region    = var.aws_region
   environment   = terraform.workspace
 }
 
@@ -22,5 +22,22 @@ module "aws-instance" {
   source         = "../modules/ec2"
   aws_region       = local.aws_region
   instance_type  = local.instance_type
-  instance_count = 2
+  instance_count = var.instance_count
+}
+
+
+module "vpc" {
+  source         = "../modules/networking"
+  name = var.name
+  cidr = var.cidr
+  azs             = var.azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+  enable_nat_gateway = var.enable_nat_gateway
+  access_ip=var.access_ip
+
+  tags = {
+    Terraform = "true"
+    Environment = terraform.workspace
+  }
 }
