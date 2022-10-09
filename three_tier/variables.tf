fcf843e55762fb85a6aef25b5ea5bd6d4a495c12
gkranasinghe <gkranasinghe@gmail.com>
2022-10-09 19:42:50 +0530

    cleanup variables

--- three_tier/variables.tf
+++ three_tier/variables.tf
@@ -1,137 +1,24 @@
 
-variable aws_region{
-  type = string
-}
-variable aws_account_id{
-  type = string
-}
+variable aws_region{}
+variable aws_account_id{}
 
 #########################################################
 #  variables relating to networkign 
 #########################################################
 
-variable "access_ip" {
-  type    = string
-}
-variable "name" {
-  type    = string
-}
-variable "public_subnets" {
-  description = "A list of public subnets inside the VPC"
-  type        = list(string)
-  default     = []
-}
-variable "private_subnets" {
-  description = "A list of private subnets inside the VPC"
-  type        = list(string)
-  default     = []
-}
-variable "private_subnet_suffix" {
-  description = "Suffix to append to private subnets name"
-  type        = string
-  default     = "private"
-}
-variable "database_subnets" {
-  description = "A list of database subnets"
-  type        = list(string)
-  default     = []
-}
-variable "azs" {
-  description = "A list of availability zones names or ids in the region"
-  type        = list(string)
-  default     = []
-}
-variable "cidr" {
-  description = "(Optional) The IPv4 CIDR block for the VPC. CIDR can be explicitly set or it can be derived from IPAM using `ipv4_netmask_length` & `ipv4_ipam_pool_id`"
-  type        = string
-  default     = "0.0.0.0/0"
-}
-variable "nat_gateway_destination_cidr_block" {
-  type        = string
-  default     = "0.0.0.0/0"
-}
-
-variable "enable_dns_hostnames" {
-  description = "Should be true to enable DNS hostnames in the VPC"
-  type        = bool
-  default     = false
-}
-variable "enable_dns_support" {
-  description = "Should be true to enable DNS support in the VPC"
-  type        = bool
-  default     = true
-}
-variable "single_nat_gateway" {
-  description = "Should be true if you want to provision a single shared NAT Gateway across all of your private networks"
-  type        = bool
-  default     = false
-}
-variable "one_nat_gateway_per_az" {
-  description = "Should be true if you want only one NAT Gateway per availability zone. Requires `var.azs` to be set, and the number of `public_subnets` created to be greater than or equal to the number of availability zones specified in `var.azs`."
-  type        = bool
-  default     = false
-}
-variable "enable_nat_gateway" {
-  description = "Should be true if you want to provision NAT Gateways for each of your private networks"
-  type        = bool
-  default     = false
-}
-
-variable "create_igw" {
-  description = "Controls if an Internet Gateway is created for public subnets and the related routes that connect them."
-  type        = bool
-  default     = true
-}
-variable "database_subnet_group_name" {
-  description = "Name of database subnet group"
-  type        = string
-  default     = null
-}
-variable "database_subnet_suffix" {
-  description = "Suffix to append to database subnets name"
-  type        = string
-  default     = "db"
-}
-variable "create_database_subnet_route_table" {
-  description = "Controls if separate route table for database should be created"
-  type        = bool
-  default     = false
-}
-variable "tags" {
-  description = "A map of tags to add to all resources"
-  type        = map(string)
-  default     = {}
-}
-variable "public_subnet_tags" {
-  description = "Additional tags for the public subnets"
-  type        = map(string)
-  default     = {}
-}
-variable "database_subnet_tags" {
-  description = "Additional tags for the public subnets"
-  type        = map(string)
-  default     = {}
-}
-variable "vpc_tags" {
-  description = "Additional tags for the VPC"
-  type        = map(string)
-  default     = {}
-}
-variable "igw_tags" {
-  description = "Additional tags for the internet gateway"
-  type        = map(string)
-  default     = {}
-}
-variable "nat_eip_tags" {
-  description = "Additional tags for the NAT EIP"
-  type        = map(string)
-  default     = {}
-}
+variable "name" {}
+variable "cidr" {}
+variable "azs" {}
+variable "private_subnets" {}
+variable "public_subnets" {}
+variable "database_subnets" {}
+variable "enable_nat_gateway" {}
+variable "access_ip" {}
 
 #########################################################
 #  variables relating to compute module 
 #########################################################
+
 variable "key_name" {}
 variable "bastion_instance_count" {}
-# variable "instance_type" {}
-variable instance_type{}
+variable "instance_type" {}
